a974a8cce41f701b7b5c9a574101c660f0b9074b
mconstruction459-star <mconstruction459@gmail.com>
2025-08-31 21:02:08 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Hero background slideshow
-const hero = document.querySelector('.hero');
-const images = ["images/bg1.png", "images/bg2.png", "images/bg3.png"];
-let i = 0;
-
-setInterval(() => {
-  i = (i + 1) % images.length;
-  hero.style.backgroundImage = `url(${images[i]})`;
-}, 5000); // ndryshon çdo 5 sekonda
